001860b95245124865d8d1e026f760f8ef46aadc
naoya25 <onaoya2002@gmail.com>
2023-02-20 01:32:22 +0900

    first commit

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-ACCESS_TOKEN = "38Be5uBTrlqnY9d+CQToiVTEPCXHQFt7kUJJElJRtxibM/IESXmxOdrPsjo4Zyc6XVaeJ/s9OmbjoxEnrd/lIOfJohrq47bUNlr24sT1cg+G6R9CnUgEIShMz/PIW/5urANQQO8ShTwc4FvJgPzQnwdB04t89/1O/w1cDnyilFU="
-SECRET = "b318448d3c466a165e919d60399bc41a"
-
 line_bot_api = LineBotApi(ACCESS_TOKEN)
 handler = WebhookHandler(SECRET)
 
