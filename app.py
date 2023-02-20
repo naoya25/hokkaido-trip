f250e007f636d4162d3f7e67ea8a17d079a29003
naoya25 <onaoya2002@gmail.com>
2023-02-21 02:53:59 +0900

    finish

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import os
-import csv
-import datetime
 
 app = Flask(__name__)
 
@@ -27,36 +25,10 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message():
-    text = "通知テスト"
-    dt_now = datetime.datetime.now()
-    today = f"{dt_now.month}/{dt_now.day}"
-
-    with open("plan.csv", "r") as f:
-        reader = csv.reader(f)
-        for line in reader:
-            if today == line[0]:
-                if (
-                    dt_now + datetime.timedelta(minutes=30)
-                    <= datetime.datetime(
-                        int(dt_now.year),
-                        int(dt_now.month),
-                        int(dt_now.day),
-                        int(line[1].split(":")[0]),
-                        int(line[1].split(":")[1]),
-                        0,
-                    )
-                    <= dt_now + datetime.timedelta(hours=1)
-                ):
-                    text += "次のスケジュール\n"
-                    text += "----------\n"
-                    text += f"時間：{line[0]} {line[1]}\n"
-                    text += f"場所：{line[2]}\n"
-                    text += f"{line[3]}\n"
-                    text += f"内容：{line[4]}\n"
-                    text += "----------"
-    print(text)
-    line_bot_api.broadcast(messages=TextSendMessage(text=text))
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text=event.message.text)
+    )
 
 
 if __name__ == "__main__":
